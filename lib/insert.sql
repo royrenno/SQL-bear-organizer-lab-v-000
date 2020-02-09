431cf4a99a62fe6bce31cd2dfce633b26ae50f33
roy renno <royrenno@yahoo.com>
2020-02-09 01:05:07 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Mr. Chocolate', 15, 'M', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES INTO bears (name, age, gender, color, temperament, alive)
